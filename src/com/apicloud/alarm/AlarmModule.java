951df18233fc969a9b346d8d8742034a1cbf5f12
fmblzf <fmblzf@example.com>
2016-07-22 16:01:18 +0800

    创建Apicloud模块-定时器模块

---
+++ src/com/apicloud/alarm/AlarmModule.java
@@ -0,0 +1,103 @@
+package com.apicloud.alarm;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.uzmap.pkg.uzcore.UZWebView;
+import com.uzmap.pkg.uzcore.annotation.UzJavascriptMethod;
+import com.uzmap.pkg.uzcore.uzmodule.UZModule;
+import com.uzmap.pkg.uzcore.uzmodule.UZModuleContext;
+
+public class AlarmModule extends UZModule {
+	/**
+	 * 闹钟管理
+	 */
+	private AlarmManager alarm = null;
+	/**
+	 * 意图
+	 */
+	private Intent intent = null;
+
+	public AlarmModule(UZWebView webView) {
+		super(webView);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 启动闹钟
+	 * 
+	 * @param moduleContext
+	 */
+	@UzJavascriptMethod
+	public void jsmethod_start(UZModuleContext moduleContext) {
+		AlarmReceiver.uzContext = moduleContext;
+		AlarmReceiver.isCancel = false;
+		int delayTime = moduleContext.optInt("delayTime");
+		int interval = moduleContext.optInt("interval");
+		// 系统真是时间
+		Log.e("闹钟", "开始......");
+		long firstime = SystemClock.elapsedRealtime();
+		if (alarm == null)
+			alarm = (AlarmManager) mContext
+					.getSystemService(mContext.ALARM_SERVICE);
+		intent = new Intent(mContext, AlarmReceiver.class);
+		intent.setAction("alerm");
+		PendingIntent sender = PendingIntent.getBroadcast(mContext, 0, intent,
+				PendingIntent.FLAG_UPDATE_CURRENT);
+		alarm.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstime
+				+ delayTime, interval, sender);
+		Log.e("闹钟", "启动成功！");
+	}
+
+	/**
+	 * 停止闹钟
+	 * 
+	 * @param moduleContext
+	 */
+	@UzJavascriptMethod
+	public void jsmethod_stop(UZModuleContext moduleContext) {
+		AlarmReceiver.isCancel = true;
+		AlarmReceiver.uzContext = moduleContext;
+		stopAlarm();
+	}
+
+	private void stopAlarm() {
+		if (alarm != null) {			
+			alarm.cancel(PendingIntent.getBroadcast(mContext, 0, intent,
+					PendingIntent.FLAG_CANCEL_CURRENT));
+		}
+		Log.e("闹钟", "取消闹钟");
+
+	}
+
+	/**
+	 * 获取gps数据
+	 * 
+	 * @param moduleContext
+	 */
+	@UzJavascriptMethod
+	public void jsmethod_check(UZModuleContext moduleContext) {
+		String moduString = moduleContext.optString("message");
+		if (moduString == null) {
+			AlarmReceiver.isCancel = true;
+			AlarmReceiver.uzContext = moduleContext;
+			stopAlarm();
+		}
+		Toast.makeText(mContext, moduString, Toast.LENGTH_SHORT).show();
+	}
+
+	/**
+	 * 获取gps数据
+	 * 
+	 * @param moduleContext
+	 */
+	@UzJavascriptMethod
+	public void jsmethod_getgps(UZModuleContext moduleContext) {
+
+	}
+
+}
---
+++ src/com/apicloud/alarm/AlarmReceiver.java
@@ -0,0 +1,31 @@
+package com.apicloud.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.uzmap.pkg.uzcore.uzmodule.UZModuleContext;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+	public static UZModuleContext uzContext = null;
+	public static boolean isCancel = false;
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		
+//		Toast.makeText(context, "闹钟...", Toast.LENGTH_SHORT).show();
+		Log.e("闹钟", "c测试");
+		if(uzContext != null){
+			if(isCancel){
+				uzContext.success("取消闹钟",false,false);	
+			}else{
+				uzContext.success("成功回调",false,false);	
+			}					
+		}
+	}
+
+}
